568c77a878d58605b3a21c96eb5502f89ad8071a
Shahar Solomianik <shahar@gnift.com>
2015-12-30 09:45:28 +0200

    f

--- test.js
+++ test.js
@@ -77,3 +77,37 @@ function avichay4(shahar){
 function avichay5(shahar){
 	return lior;
 }
+
+
+/*
+*
+* @slack I believe this function can be used globally, please let me know what you think #reuse
+*
+*/
+function parseParams (cmd, args, cb) {
+  var params = {}
+  if (cmd === 'grant') {
+    params.permissions = args.shift()
+  }
+  if (['grant', 'revoke', 'ls-packages'].indexOf(cmd) !== -1) {
+    var entity = (args.shift() || '').split(':')
+    params.scope = entity[0]
+    params.team = entity[1]
+  }
+  getPackage(args.shift(), function (err, pkg) {
+    if (err) { return cb(err) }
+    params.package = pkg
+
+    if (!params.scope && cmd === 'ls-packages') {
+      whoami([], true, function (err, scope) {
+        params.scope = scope
+        cb(err, params)
+      })
+    } else {
+      if (cmd === 'ls-collaborators') {
+        params.user = args.shift()
+      }
+      cb(null, params)
+    }
+  })
+}
